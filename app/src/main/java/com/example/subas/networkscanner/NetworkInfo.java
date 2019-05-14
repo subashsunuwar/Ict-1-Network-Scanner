3db10100d32938f97e03187191c964ecb1200ff7
subashsunuwar <subash.sunuwar@my.jcu.edu.au>
2019-05-14 20:40:57 +1000

    network activity created and network button function added

---
+++ app/src/main/java/com/example/subas/networkscanner/NetworkInfo.java
@@ -0,0 +1,13 @@
+package com.example.subas.networkscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NetworkInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_network_info);
+    }
+}
