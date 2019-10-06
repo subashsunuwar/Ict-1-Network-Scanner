98be4fa8f6bc52a6176eb66355dc7124ba8ff1c4
subashsunuwar <subash_sunuwar@hotmail.com>
2019-10-06 23:05:28 +1000

    design changed from splash screen to layout

--- app/src/main/java/com/android/networkscanner/SplashActivity.java
+++ app/src/main/java/com/android/networkscanner/SplashActivity.java
@@ -12,7 +12,7 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
@@ -24,12 +24,13 @@ public class SplashActivity extends Activity {
                 startActivity(i);
                 finish();
             }
-        }, 1*1000);*/
+        }, 1*1000);
 
+        /*
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
-
+        */
 
     }
 
